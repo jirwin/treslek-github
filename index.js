e2142ca23e9efe2d9b1ef2c5792a915f9979b75c
Justin Gallardo <justin.gallardo@gmail.com>
2016-03-27 01:28:45 -0700

    Add support for searching github issues on a repo. Thanks jjbuchan.

--- index.js
+++ index.js
@@ -7,8 +7,70 @@ var GITHUB_USER = process.env.TRESLEK_GITHUB_USER;
 var GITHUB_TOKEN = process.env.TRESLEK_GITHUB_TOKEN;
 
 var Github = function() {
-  this.commands = ['issue'];
+  this.commands = ['issue', 'issue-search'];
   this.auto = ['load'];
+};
+
+Github.prototype._parseMsg = function(msg) {
+  var msgParts = msg.split(' '),
+      userRepo = msgParts[0].split('/'),
+      realMsg = msg.slice(msgParts[0].length, msg.length);
+
+  return {
+    user: userRepo[0],
+    repo: userRepo[1],
+    msg: realMsg
+  };
+};
+
+// Borrowed from https://github.com/jjbuchan/treslek-gh-issue-search/blob/master/index.js
+Github.prototype['issue-search'] = function(bot, to, from, msg, callback) {
+  var parsedMsg = this._parseMsg(msg);
+
+  if (!parsedMsg.user || !parsedMsg.repo) {
+    bot.say(to, 'You must provide a user and repo to create an issue on.');
+    callback();
+    return;
+  }
+
+  github.search.issues({
+    q: sprintf('repo:%s/%s+%s', parsedMsg.user, parsedMsg.repo, parsedMsg.msg)
+  }, function(err, results) {
+    if (err) {
+      bot.say(to, 'Unable to perform search.');
+      callback();
+      return;
+    }
+
+    var totalCount = 0,
+        issueTitles = [],
+        outMsg = '';
+
+    totalCount = results.total_count;
+
+    if (totalCount) {
+      results.items.forEach(function(item) {
+        issueTitles.push({
+          title:item.title,
+          url: item.html_url
+        });
+      });
+
+      outMsg = sprintf('%d issues found:', totalCount);
+      issueTitles.forEach(function(item) {
+        outMsg += sprintf('\n - %s: %s', item.title, item.url);
+      });
+    } else {
+      outMsg = 'No issues were found for search: ' + msg;
+    }
+
+    if (totalCount > 5) {
+      bot.say(from, outMsg);
+    } else {
+      bot.say(to, outMsg);
+    }
+    callback();
+  });
 };
 
 Github.prototype.issue = function(bot, to, from, msg, callback) {
@@ -25,17 +87,16 @@ Github.prototype.issue = function(bot, to, from, msg, callback) {
     };
   }
 
-  var msgParts = msg.split(' ');
-  var userRepo = msgParts[0].split('/');
+  var parsedMsg = this._parseMsg(msg);
 
-  if (userRepo.length !== 2) {
+  if (!parsedMsg.user || !parsedMsg.repo) {
     bot.say(to, 'You must provide a user and repo to create an issue on.');
     callback();
     return;
   }
 
   var titleBody = {};
-  var tbMsg = msg.slice(msgParts[0].length, msg.length);
+  var tbMsg = parsedMsg.msg;
   if (tbMsg.length < 80) {
     titleBody.title = tbMsg;
     titleBody.body = '';
